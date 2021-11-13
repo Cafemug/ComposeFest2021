010aa6df5f36cb09ac29076ca6b50314f4d4515f
cafemug <hyanghope@gmail.com>
2021-11-13 22:14:41 +0900

    fix: intreinsics

--- week 2-1-Layouts in JetpackCompose/Lab02-1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/Lab02-1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -410,3 +410,36 @@ fun ConstraintLayoutContentPreview() {
         ConstraintLayoutContent()
     }
 }
+
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(color = Color.Black, modifier = Modifier.fillMaxHeight().width(1.dp))
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+
+            text = text2
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    MyApplicationTheme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
+    }
+}
