d1e2c786b7d5a9980baf3b41a15901c8a0810b46
cafemug <hyanghope@gmail.com>
2021-11-27 23:42:27 +0900

    fix: viewcompositionStrategy interop theming

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,6 +16,7 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.content.res.Configuration
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.compose.foundation.layout.Column
@@ -38,6 +39,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -83,15 +85,37 @@ private fun PlantName(name: String) {
             .wrapContentWidth(Alignment.CenterHorizontally)
     )
 }
-
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PlantDetailContentDarkPreview() {
+    val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
+    MdcTheme {
+        PlantDetailContent(plant)
+    }
+}
 @Preview
 @Composable
 private fun PlantNamePreview() {
-    MaterialTheme {
+    MdcTheme {
         PlantName("Apple")
     }
 }
 
+@Preview
+@Composable
+private fun PlantWateringPreview() {
+    MdcTheme {
+        PlantWatering(7)
+    }
+}
+
+@Preview
+@Composable
+private fun PlantDescriptionPreview() {
+    MdcTheme {
+        PlantDescription("HTML<br><br>description")
+    }
+}
 @Composable
 private fun PlantWatering(wateringInterval: Int) {
     Column(Modifier.fillMaxWidth()) {
@@ -119,13 +143,7 @@ private fun PlantWatering(wateringInterval: Int) {
     }
 }
 
-@Preview
-@Composable
-private fun PlantWateringPreview() {
-    MaterialTheme {
-        PlantWatering(7)
-    }
-}
+
 
 @Composable
 private fun PlantDescription(description: String) {
@@ -148,10 +166,3 @@ private fun PlantDescription(description: String) {
     )
 }
 
-@Preview
-@Composable
-private fun PlantDescriptionPreview() {
-    MaterialTheme {
-        PlantDescription("HTML<br><br>description")
-    }
-}
