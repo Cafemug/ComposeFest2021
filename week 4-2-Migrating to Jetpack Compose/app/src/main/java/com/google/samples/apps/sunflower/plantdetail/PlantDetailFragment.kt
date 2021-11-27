c083a7c7371d778bdb31e4e9cce2802f6ff13e5d
cafemug <hyanghope@gmail.com>
2021-11-27 23:31:14 +0900

    fix: viewmodel and live data

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -70,9 +70,8 @@ class PlantDetailFragment : Fragment() {
                 }
             }
             composeView.setContent {
-                // You're in Compose world!
                 MaterialTheme {
-                    PlantDetailDescription()
+                    PlantDetailDescription(plantDetailViewModel)
                 }
             }
             var isToolbarShown = false
